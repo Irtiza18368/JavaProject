3e93c3b8a5f2312cebc2947c5919d1e48d62cacb
Irtiza18368 <Irtiza12550@gmail.com>
2023-07-18 21:25:22 +0100

    Hello World Part 1

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("My name is C3PO");
+        System.out.println("Where is R2D2");
+    }
+}
