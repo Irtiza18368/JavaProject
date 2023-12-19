f534da8c8e60537d1ca8d18dccd1fca988de51f1
Irtiza18368 <Irtiza12550@gmail.com>
2023-12-19 14:27:45 +0000

    Practice purpose

---
+++ src/practice.java
@@ -0,0 +1,11 @@
+class HelloMessage
+{
+    public static void main(String[]param){
+        hellomessage();
+        System.exit(0);
+    }
+    public static void hellomessage()
+    {
+        System.out.println("Hello World");
+    }
+}
